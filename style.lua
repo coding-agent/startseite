c0aff265fdf2babc42b5d0a23543d78e0a4de0ae
mcmrcs@proton.me <marciusdasilva51@gmail.com>
2024-05-07 18:53:19 +0100

    chore: Styling White and Dark Mode

--- style.lua
+++ style.lua
@@ -1,9 +1,12 @@
 local style = {}
 
-style.background = '#202020'
+style.dark_background = '#202020'
+style.white_background = '#FFFFFF'
 style.sub_background = '#E1E1E1'
-style.menu_background = '#181818'
-style.text = '#FFFFFF'
+style.dark_menu_background = '#181818'
+style.white_menu_background = '#D1D1D1'
+style.white_text = '#F1F1F1'
+style.dark_text = '#303030'
 style.sub_text = '#F7A41D'
 style.blue = '#8888FF'
 style.green = '#21EA55'

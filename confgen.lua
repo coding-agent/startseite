91cf97e449783d6e9ab0b7105061c9e2bf6a0a23
mcmrcs@proton.me <marciusdasilva51@gmail.com>
2024-06-02 22:25:24 +0100

    ci: order articles in post evaluation

--- confgen.lua
+++ confgen.lua
@@ -3,19 +3,7 @@ cg.addPath('scripts')
 cg.addFile('index.html.cgt')
 cg.addFile('style.css.cgt')
 
-local function getArticles()
-    local list = {}
-
-    for file in io.popen('ls articles/'):lines() do
-        if file:sub(-4) == ".lua" then
-            local article = loadfile("articles/" .. file)()
-            if article then
-                article.file = file:gsub("%.lua$", ".html")
-                list[file:gsub("%.lua$", "")] = article
-            end
-        end
-    end
-
+local function orderArticles(list)
     local function dateToNumber(date_str)
         local day, month, year = date_str:match("(%d%d)-(%d%d)-(%d%d%d%d)")
         return tonumber(year .. month .. day)
@@ -28,9 +16,27 @@ local function getArticles()
     return list
 end
 
-cg.opt.articles = getArticles();
-cg.opt.style = require('style')
+function GetArticles()
+    local list = {}
 
+    for file in io.popen('ls articles/'):lines() do
+        if file:sub(-4) == ".lua" then
+            local article = loadfile("articles/" .. file)()
+            if article then
+                article.file = file:gsub("%.lua$", ".html")
+                list[file:gsub("%.lua$", "")] = article
+            end
+        end
+    end
+
+    return orderArticles(list)
+end
+
+local email = "info@coding-agent.me"
+local github = "https://github.com/coding-agent/"
+
+cg.opt.highlight = require("highlight")
+cg.opt.style = require('style')
 cg.opt.doctype = "<!doctype html>"
 cg.opt.header_links = [[
 <meta charset="UTF-8">
@@ -46,10 +52,6 @@ cg.opt.header_links = [[
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Share+Tech&display=swap" rel="stylesheet">
 ]]
-
-email = "info@coding-agent.me"
-github = "https://github.com/coding-agent/"
-
 cg.opt.menu = [[
 <div class="menu">
     <a href="index.html"><i class="fa-solid fa-house"></i> Home </a>
@@ -57,5 +59,3 @@ cg.opt.menu = [[
     <a href="]].. github ..[["> <i class="fa-brands fa-github"></i> Github </a>
 </div>
 ]]
-
-cg.opt.highlight = require("highlight")

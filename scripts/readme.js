fe84bb95d95874532b5a67c09b3f8eba8b2e2b9e
mcmrcs@proton.me <marciusdasilva51@gmail.com>
2024-06-02 18:31:23 +0100

    bugfix: fix readme script

--- scripts/readme.js
+++ scripts/readme.js
@@ -1,15 +1,19 @@
-document.querySelector("div.article").parentElement.onmouseover = function (e) {
-  const el = e.target
-  const child = el.querySelector(".blinking")
-  if (child != null) {
-    child.style.display = "block"
+document.querySelectorAll("div.article").forEach((el) => {
+  el.parentElement.onmouseover = function (e) {
+    const target = e.target
+    const child = target.querySelector(".blinking")
+    if (child != null) {
+      child.style.display = "block"
+    }
   }
-}
+})
 
-document.querySelector("div.article").parentElement.onmouseleave = function (e) {
-  const el = e.target
-  const child = el.querySelector(".blinking")
-  if (child != null) {
-    child.style.display = "none"
+document.querySelectorAll("div.article").forEach((el) => {
+  el.parentElement.onmouseleave = function (e) {
+    const target = e.target
+    const child = target.querySelector(".blinking")
+    if (child != null) {
+      child.style.display = "none"
+    }
   }
-}
+})
